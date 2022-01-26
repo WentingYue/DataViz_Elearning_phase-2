5a7664680f605c8f89f0ff3e7fe76eae5ed164f4
Joli Holmes <holmesjoli@gmail.com>
2022-01-26 13:57:32 -0500

    moved functions to the helper_functions.js file to keep code more modular

--- visualization/js/network.js
+++ visualization/js/network.js
@@ -126,29 +126,6 @@ d3.queue()
         var radius_b = 0.013;
         var radius_c = 0.03;
 
-        // real-time calculation
-        function aggregate(array) {
-            var obj = {};
-            array.forEach(function (val) {
-                if (!obj[val])
-                    obj[val] = 1;
-                else
-                    obj[val]++;
-            });
-            return obj;
-        }
-
-        function aggregateScore(array, score) {
-            var obj = {};
-            array.forEach(function (val) {
-                if (!obj[val.key])
-                    obj[val.key] = val[score];
-                else
-                    obj[val.key] = val[score] + obj[val.key];
-            });
-            return obj;
-        }
-
         g = graphdata;
         store = $.extend(true, {}, graphdata);
 
